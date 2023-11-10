9250733d8508cf2e07fc402d26c6b1efb7e0f519
dany-boop <dny77853@gmail.com>
2023-11-10 17:15:32 +0700

    ...

--- utils/time.ts
+++ utils/time.ts
@@ -1,12 +1,18 @@
 import { DateTime } from 'luxon';
 
 export const getTime = () => {
-  const past = DateTime.fromObject(
-    JSON.parse(process.env.DATE_TIME as unknown as string),
-    { zone: process.env.DATE_TIMEZONE }
-  );
-  const now = DateTime.now();
+  try {
+    console.log('DATE_TIME:', process.env.DATE_TIME);
+    const past = DateTime.fromObject(
+      JSON.parse(process.env.DATE_TIME as string),
+      { zone: process.env.DATE_TIMEZONE }
+    );
+    const now = DateTime.now();
 
-  const diff = now.diff(past, ['years']);
-  return Math.floor(diff.years);
+    const diff = now.diff(past, ['years']);
+    return Math.floor(diff.years);
+  } catch (error) {
+    console.error('Error parsing DATE_TIME:', error);
+    return 0; // or handle the error in a way that fits your application
+  }
 };
